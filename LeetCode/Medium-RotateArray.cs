4b07262abf8ac8a05ea08e1da5826c89c5ec2d63
David Bourke <davidrbourke@gmail.com>
2025-03-08 17:40:51 +0000

    Adding Rotate Array solution

--- LeetCode/Medium-RotateArray.cs
+++ LeetCode/Medium-RotateArray.cs
@@ -1,5 +1,27 @@
 public class Solution {
     public void Rotate(int[] nums, int k) {
+        if (nums.Length == 1 || k == 0 || k == nums.Length)
+            return;
         
+        k %= nums.Length;
+
+        int pos = 0;
+        int nextValueWrite = nums[pos];
+        int srcIndex = pos+k;
+        for(var i = 0; i < nums.Length; i++)
+        {
+            var nextValueRead = nums[srcIndex];
+            nums[srcIndex] = nextValueWrite;
+            nextValueWrite = nextValueRead;
+
+            if (srcIndex == pos) {
+                srcIndex++;
+                pos++;
+                nextValueWrite = nums[pos];
+            }
+
+            srcIndex = (srcIndex +k) % nums.Length;
+        }
+
     }
 }
